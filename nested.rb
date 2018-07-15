53ab573fdb924b0310741fe352631b28af42d5db
Sharon Mun <sharonymun@gmail.com>
2018-07-15 19:19:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][la]
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  
+    programmer_hash
 end
